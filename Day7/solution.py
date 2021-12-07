75705862491798744f4509fcfdc51bbae1479853
Cronvall <47293083+Cronvall@users.noreply.github.com>
2021-12-07 19:47:52 +0100

    ---

--- Day7/solution.py
+++ Day7/solution.py
@@ -1,25 +1,47 @@
+import statistics
 
 FILE = open("input.txt", 'r').read().split(',')
-
+IP = []
+for obj in FILE:
+    IP.append(int(obj))
+IP.sort()
 
 def solution1():
-    ip = []
-    for obj in FILE:
-        ip.append(int(obj))
-    ip.sort()
-    median = ip[int(len(ip) / 2)]
+    median = IP[int(len(IP) / 2)]
 
     fuelCost = 0
-    for obj in ip:
+    for obj in IP:
         fuelCost += abs(obj - median)
     print(fuelCost)
 
+def solution2(list, position):
+    fueltot = 0
+    for i in list:
+        fueltot += sum(fuelcalc(abs(position - i)))
+    result = fueltot, position
+    return  result
 
-def solution2():
-    #TODO
-    pass
 
+#Used for Solution2
+def fuelcalc(rng):
+    sum_lst = []
+    counter = 0
+    for i in range(rng):
+        counter += 1
+        sum_lst.append(counter)
+    return  sum_lst
+
+
+#Used to solve Solution2 (Very diry way to do it)
+def superDuperLoop():
+    result =  solution2(IP, round(statistics.mean(IP)))
+    for i in range(len(IP)):
+        newSol = solution2(IP, i)
+        if newSol[0] < result[0]:
+            result = newSol
+    return result
 
 
 if __name__ == "__main__":
-    solution2()
+    solution1()
+    print(superDuperLoop())
